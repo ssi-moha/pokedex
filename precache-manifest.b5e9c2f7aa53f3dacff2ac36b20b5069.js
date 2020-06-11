d495749454a9e83f2ba767c258bab9b38f954bf6
Sinane <ssi-moha@student.42.fr>
2020-06-11 23:40:33 +0200

    Updates

---
+++ precache-manifest.b5e9c2f7aa53f3dacff2ac36b20b5069.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c741184a4df8aadb9988f2c182f3caca",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "0600b0c5d2af61145976",
+    "url": "/pokedex/static/css/2.22d6b5ec.chunk.css"
+  },
+  {
+    "revision": "c542b9d21b6230dc4813",
+    "url": "/pokedex/static/css/main.30cd6294.chunk.css"
+  },
+  {
+    "revision": "0600b0c5d2af61145976",
+    "url": "/pokedex/static/js/2.75e1621c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pokedex/static/js/2.75e1621c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c542b9d21b6230dc4813",
+    "url": "/pokedex/static/js/main.f0e0295a.chunk.js"
+  },
+  {
+    "revision": "ed5a9742913badf7a85c",
+    "url": "/pokedex/static/js/runtime-main.82622ded.js"
+  }
+]);
